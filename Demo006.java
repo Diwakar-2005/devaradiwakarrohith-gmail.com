ba532a63d02592487b9bf03d2984411e329c8daf
Diwakar-2005 <devaradiwakarrohith@gmail.com>
2025-02-04 12:38:12 +0530

    Update Demo006.java

--- Demo006.java
+++ Demo006.java
@@ -1,8 +1,8 @@
 class Demo006{
     public static void main(String[] args){
-        String username="jashmika_001";
-        String pwd="jashmikaa_001";
-        while((username=="jashmika_001")&&(pwd=="jashmikaa_001")){
+        String username="DDR_001";
+        String pwd="DDR_001";
+        while((username=="DDR_001")&&(pwd==DDR_001")){
             System.out.println("hello world");
             break;
         }
